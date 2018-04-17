90555d7bd4b1ac68bd6f5fffb39b9b6edc7d7ba2
w0l00qx <wli@walmartlabs.com>
2018-04-17 16:01:33 -0700

    Fixed show modal

--- index.android.js
+++ index.android.js
@@ -1,7 +1,8 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import PushScreen from './PushScreen';
+import ShowModal FROM './ShowModal';
 
 AppRegistry.registerComponent('NavDemoMiniApp', () => App);
 AppRegistry.registerComponent('NavDemoMiniApp/push_screen', () => PushScreen);
-AppRegistry.registerComponent('NavDemoMiniApp/modal_Screen', () => PushScreen);
+AppRegistry.registerComponent('NavDemoMiniApp/modal_Screen', () => ShowModal);
--- index.ios.js
+++ index.ios.js
@@ -1,8 +1,9 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import PushScreen from './PushScreen';
+import ShowModal from './ShowModal';
 
 
 AppRegistry.registerComponent('NavDemoMiniApp', () => App);
 AppRegistry.registerComponent('NavDemoMiniApp/push_screen', () => PushScreen);
-AppRegistry.registerComponent('NavDemoMiniApp/modal_Screen', () => PushScreen);
+AppRegistry.registerComponent('NavDemoMiniApp/show_modal', () => ShowModal);
