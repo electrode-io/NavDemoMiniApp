b75106d8f8d429380a40a732f3baf1c6425222d3
Deepu Eswarath Ganapathiyadan <DGanapathiyadan@walmartlabs.com>
2018-05-25 14:39:24 -0700

    fix import

--- index.android.js
+++ index.android.js
@@ -3,11 +3,11 @@ import App from './App';
 import PushScreen from './PushScreen';
 import ShowModal from './ShowModal';
 import ShowToast from './ShowToast';
-import CustomButtons from '/CustomButtons';
+import CustomButtons from './CustomButtons';
 
 
 AppRegistry.registerComponent('NavDemoMiniApp', () => App);
 AppRegistry.registerComponent('NavDemoMiniApp/push_screen', () => PushScreen);
 AppRegistry.registerComponent('NavDemoMiniApp/show_modal', () => ShowModal);
 AppRegistry.registerComponent('NavDemoMiniApp/show_toast', () => ShowToast);
-AppRegistry.registerComponent('NavDemoMiniApp/custom_button', () => CuttomButtons);
+AppRegistry.registerComponent('NavDemoMiniApp/custom_button', () => CustomButtons);
